0f45d30fde08b9a18741655616288afc1990ba1e
sergeyorolov <korolovsergio@mail.ru>
2021-07-07 12:36:53 +0300

    add news block

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	$('.slider__inner').slick({
+	$('.slider__inner, .news__slider-inner').slick({
 		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
 		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
 		infinite: false
